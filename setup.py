b6de37ea33607b3f6846192de4368c6e7d74ef9a
wmagdum <wmagdum@gmail.com>
2024-04-17 16:43:33 +0530

    setup and requiremet

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages,setup
+
+from typing import List
+
+HYPEN_E_DOT='-e .'
+def get_requirements(file_path:str)->List[str]:
+    '''
+    this function will return list of requirements
+
+    '''
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n","") for req in requirements]
+        
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+
+
+setup(
+name='student_data',
+version='0.0.1',
+author='wasim',
+author_email='wmagdum@gmail.com',
+packages=find_packages(),
+install_requires=get_requirements("requirements.txt")
+)
